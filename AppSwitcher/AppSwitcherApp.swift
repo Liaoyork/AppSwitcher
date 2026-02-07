ec9b5cb53d116b724fde522f23e920609d70b378
York <ymes0405@gmail.com>
2026-02-07 12:35:38 +0800

    Initial Commit

---
+++ AppSwitcher/AppSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwitcherApp.swift
+//  AppSwitcher
+//
+//  Created by 廖育瑲 on 2026/2/7.
+//
+
+import SwiftUI
+
+@main
+struct AppSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppSwitcher
+//
+//  Created by 廖育瑲 on 2026/2/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
